1e84cfdd1af214e6f736b9da644cc75ef083859a
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2022-10-26 20:25:00 +0100

    Tuples Exercises

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# Tuples
+
+from unicodedata import name
+
+
+names = ('Roger', 'Syd', 'Bob')
+
+names[0]
+names.index('Roger')
+
+
+len(names)
+
+print('Roger' in names)
+
+names[0:2]
+
+print(sorted(names))
+
+ 
